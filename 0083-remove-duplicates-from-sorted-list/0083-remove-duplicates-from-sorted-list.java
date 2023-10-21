c99cd9b74253e83d925168bca4a394d048140026
Debapriyo Das <debapriyo00@gmail.com>
2023-10-21 10:04:00 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (17.11%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -13,15 +13,15 @@ class Solution {
         if(head == null || head.next == null)return head;
         ListNode prev = head;
         ListNode curr = head.next;
-            while(curr!= null){
-                if(curr.val == prev.val){
-                    prev.next = curr.next;
-                    curr = curr.next;
-                }else{
-                    prev = prev.next;
-                    curr = curr.next;
-                }
+        while(curr!=null){
+            if(prev.val == curr.val){
+                prev.next = curr.next;
+                curr = curr.next;
+            }else{
+                prev = prev.next;
+                curr = curr.next;
             }
-        return head;
+        }
+         return head;  
     }
 }
