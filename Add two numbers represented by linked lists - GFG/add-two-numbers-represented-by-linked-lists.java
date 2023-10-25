e89e28d8a6b34517017bed5282ad95a187235c66
Debapriyo Das <debapriyo00@gmail.com>
2023-10-25 21:00:35 +0530

    Added solution - LeetHub

---
+++ Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.java
@@ -0,0 +1,119 @@
+//{ Driver Code Starts
+// driver
+
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class GfG{
+    
+    static void printList(Node n){
+        while(n!=null){
+            System.out.print(n.data+" ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        
+        while (T-- > 0) {
+            
+            int n = sc.nextInt();
+            int val = sc.nextInt();
+            
+            Node first = new Node(val);
+            Node tail = first;
+            for(int i=0; i<n-1; i++)
+            {
+                val = sc.nextInt();
+                tail.next = new Node(val);
+                tail = tail.next;
+            }
+            
+            int m = sc.nextInt();
+            val = sc.nextInt();
+            
+            Node second = new Node(val);
+            tail = second;
+            for(int i=0; i<m-1; i++)
+            {
+                val = sc.nextInt();
+                tail.next = new Node(val);
+                tail = tail.next;
+            }
+            
+            Solution g = new Solution();
+            Node res = g.addTwoLists(first, second);
+            printList(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/* node for linked list
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+*/
+
+class Solution{
+    //Function to add two numbers represented by linked list.
+    static Node addTwoLists(Node first, Node second){
+       Node left = reverse(first);
+       Node right = reverse(second);
+       Node dummy = new Node(-1);
+       Node tem = dummy;
+       int c = 0;
+       while(left!=null || right!=null){
+           int sum = 0 + c;
+           if(left!=null){
+               sum = sum + left.data;
+               left = left.next;
+           }
+           if(right!=null){
+               sum = sum+right.data;
+               right = right.next;
+           }
+           c = sum/10;
+           sum = sum % 10;
+           tem.next = new Node(sum);
+           tem = tem.next;
+       }
+       if(c == 1)tem.next = new Node(1);
+       return reverse(dummy.next);
+    }
+    //reverse function.''
+    public static Node reverse(Node head){
+        Node curr = head;
+        Node prev = null;
+        Node next;
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
