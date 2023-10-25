1612ebb7f01140db09e7073ccb4b47d90533b720
Debapriyo Das <debapriyo00@gmail.com>
2023-10-25 19:58:14 +0530

    Time: 11 ms (40.22%), Space: 56 MB (45.02%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -11,46 +11,52 @@
 class Solution {
     public ListNode sortList(ListNode head) {
         if(head == null || head.next == null)return head;
-        ListNode mid = findMid(head);
-        
-        ListNode righthead = mid.next;
+        //Step 1 : Find middle Node.
+        ListNode mid = getMid(head);
+        //Step 2: Separate left and rigth half.
+        ListNode rightHead = mid.next;
         mid.next = null;
-        ListNode leftHalf = sortList(head);
-        ListNode rightHalf = sortList(righthead);
-        return mergerList(leftHalf, rightHalf);
-    }
-    public static ListNode findMid(ListNode head){
+        ListNode newLeft = sortList(head);
+        ListNode newRight = sortList(rightHead);
+        
+        //Step 3: Merge two sorted List.
+        return mergeTwoSortedList(newLeft,newRight);
+   }
+    
+    //GetMid methord.
+    public static ListNode getMid(ListNode head){
         ListNode slow = head;
         ListNode fast = head.next;
-        while(fast!= null && fast.next!= null){
+        while(fast!=null && fast.next!= null){
             slow = slow.next;
             fast = fast.next.next;
         }return slow;
     }
-    public static ListNode mergerList(ListNode l1 , ListNode l2){
+    //mergeTwoSortedList Methord.
+    public static ListNode mergeTwoSortedList(ListNode l1, ListNode l2){
         ListNode dummy = new ListNode(-1);
         ListNode tem = dummy;
-        while(l1!= null && l2!=null){
-            if(l1.val <= l2.val){
+        while(l1!= null && l2!= null){
+            if(l1.val<=l2.val){
                 tem.next = l1;
-                tem = tem.next;
                 l1 = l1.next;
+                tem = tem.next;
             }else{
                 tem.next = l2;
-                tem = tem.next;
                 l2 = l2.next;
+                tem = tem.next;
             }
         }
         while(l1!=null){
             tem.next = l1;
-            tem = tem.next;
             l1 = l1.next;
-        }
-         while(l2!=null){
-            tem.next = l2;
             tem = tem.next;
+        }
+        while(l2!=null){
+            tem.next = l2;
             l2 = l2.next;
-        } return dummy.next;
+            tem = tem.next;
+        }return dummy.next;
     }
-   
+    
 }
