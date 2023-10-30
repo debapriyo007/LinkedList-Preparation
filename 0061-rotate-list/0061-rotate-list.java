8f90f1975b800ff096146ca3efe02bb2399125a0
Debapriyo Das <debapriyo00@gmail.com>
2023-10-30 10:14:22 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (43.13%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,32 +10,22 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        
-        //base case.
-        if(head == null || head.next == null || k == 0)return head;
-        //Step 1: cal the size of ll.
+        if(head == null || head.next == null || k==0)return head;
         int len = 1;
         ListNode curr = head;
-        while(curr.next!= null){
+        while(curr.next!=null){
             curr = curr.next;
             len++;
         }
-        //Now my curr pointing last node.
         curr.next = head;
-        //Step 2 : find out the k .
-         k = k%len;
-         k = len - k;
-        
-        //step 3: Move my curr to 3 node.  // 1 ->2->3->4->5 
+        k = k%len;
+        k = len - k;
         while(k>0){
             curr = curr.next;
             k--;
         }
-        
-        //step 4: declear my head as my curr next node
         head = curr.next;
-        curr.next = null; //break my link.
-        
+        curr.next = null;
         return head;
     }
 }
